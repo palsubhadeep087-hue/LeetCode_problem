4625859c0569be4909d7055d45b03b01507c7039
Subhadeep Pal <palsubhadeep087@gmail.com>
2026-08-24 08:24:30 +0530

    Time: 108 ms (54.91%) | Memory: 91.5 MB (76.43%) - LeetSync

---
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size();
+        vector<int>prefix(n);
+
+        prefix[0] = stones[0];
+        for(int i = 1; i < n; i++)
+        {
+            prefix[i] = prefix[i-1] + stones[i];
+        }
+        int diff = prefix[n-1];
+        for(int i = n - 2; i >= 1; i--)
+        {
+            diff = max(diff, prefix[i]-diff);
+        }
+        return diff;
+    }
+};
